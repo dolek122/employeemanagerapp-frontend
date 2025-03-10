1df7b1e9236581719f88c1663f53e5f529119eff
dolek122 <donmarines@wp.pl>
2025-03-10 19:25:20 +0100

    initial message

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,20 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
+import {environment} from '../environments/environment';
 
-
-import {Observable} from 'rxjs';
-import {Injectable} from '@angular/core';
-import {Employee} from './employee';
-import {HttpClient} from '@angular/common/http';
-
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({providedIn: 'root'})
 export class EmployeeService {
-  private apiServerUrl = "";
+  private apiServerUrl = environment.apiBaseUrl;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient){}
 
-  public getEmployee(): Observable<Employee[]> {
-    return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`)
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
   }
 
   public addEmployee(employee: Employee): Observable<Employee> {
@@ -28,5 +25,4 @@ export class EmployeeService {
   public deleteEmployee(employeeId: number): Observable<void> {
     return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
   }
-
 }
